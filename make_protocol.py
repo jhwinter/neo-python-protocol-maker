c813d78288437bd87a70adbe626567521fc8dbbd
Jonathan Winter <jonathan@splyse.tech>
2018-07-02 14:45:32 -0400

    works with the docker image

--- make_protocol.py
+++ make_protocol.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import re
 import time
+from urllib.parse import urlparse
 
 from selenium import webdriver
 from selenium.webdriver import Chrome
@@ -12,11 +12,20 @@ from selenium.webdriver import FirefoxOptions
 from make_json import write_to_json
 
 
+def get_key(seed):
+    """
+    This method returns the integer value of the node's latency to be used as the key for sorting the nodes in
+    the seed list
+    :param seed: the seed from the seed list
+    :return: integer value of the seed's latency
+    """
+    return int(seed[2])
+
+
 if __name__ == '__main__':
     """
-    This method opens the browser, gets all functional nodes, picks the very best amongst them,
-    and adds them to the protocol.json file
-    :return:
+    This method opens the browser, gets all functional nodes, sorts the list of nodes by latency,
+    and adds five of them to the protocol.json file
     """
     browser = None
     try:
@@ -39,37 +48,25 @@ if __name__ == '__main__':
     url = 'http://monitor.cityofzion.io/'
     browser.get(url)  # navigates to the url and waits for it to fully load
 
-    time.sleep(5)  # you only get like 1 node if you don't give the page time to ping the nodes again
+    time.sleep(10)  # you only get like 1 node if you don't give the page time to ping the nodes again
     possible_seed_list = []
     rows = browser.find_elements_by_class_name('color-success')  # gets all table rows with class='color-success'
-#    for row in rows:  # converting all rows from webdriver objects to strings
-#        possible_seed_list.append(row.text)
+    for row in rows:  # converting all rows from webdriver objects to strings
+        possible_seed_list.append(row.text)
 
-    def get_key(seed_latency):
-        return seed_list[2]
-
-    possible_seed_list = list(map(lambda seeda: seeda.replace('\n', ' ').split(' '), possible_seed_list))
-    possible_seed_list = list(map(lambda seed_latency: int(seed_latency[2]), possible_seed_list))
-    possible_seed_list.sort(key=get_key, reverse=True)
+    possible_seed_list = list(map(lambda seed: seed.split(' '), possible_seed_list))
+    possible_seed_list = sorted(possible_seed_list, key=get_key)  # sort the seed list by latency
     # the following is a crude implementation, but I'm not too great with regular expressions
     # so feel free to modify this to be more efficient
     seed_list = []
     for row in possible_seed_list:
-        if row[0].endswith('10332') and len(seed_list):  # only choose nodes that follow the neo standard
-            row[0] = row[0].split('://')  # remove http/https from url
-            row[0][1] = row[0][1].split(':')  # remove '10332' port from url
-            seed = row[0][1][0] + ':10333'  # append the 10333 port to the url
+        if row[0].endswith(':10332') and len(seed_list) < 5 and row[1] == 'RPC':  # only choose nodes that follow the neo standard
+            seed = urlparse(row[0]).netloc  # returns only the url part of the string such as: seed4.travala.com:10332
+            seed = seed.replace('10332', '10333')  # connect to the P2P port instead of the RPC port
+            print('seed: ', seed)
             seed_list.append(seed)
-        else:
-            break
-#        row = row.replace('\n', ' ').split(' ')  # make the formatting more consistent in each row
-#        search_url = re.search(r'http(.*)binding">(.*?):10332', row, re.M | re.I)
-#        if search_url and int(row[2]) < 250 and len(seed_list) < 5:
-#            seed_list.append(search_url.group(2)+':10333')
-#        else:
-#            break
 
-    fp = '/opt/node/neo-python/neo/data/protocol.mainnet.json'
+    fp = 'neo-python/neo/data/protocol.mainnet.json'
     if len(seed_list) > 0:
         write_to_json(seed_list, fp)
         print('neo-python SeedList has been updated')
